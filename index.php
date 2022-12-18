aec5f156925706b0963185191a07edefaf080f6f
husnunn <118254419+husnunn@users.noreply.github.com>
2022-12-18 12:34:48 +0700

    blok

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- LINK FONT AWESOME -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css' integrity='sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==' crossorigin='anonymous' />
+    <!-- LINK CSS -->
+    <link rel="stylesheet" href="style.css">
+    <title>Bakery</title>
+</head>
+
+<body>
+
+    <!-- NAVBAR START -->
+    <header>
+        <div class="navbar">
+            <div class="container">
+                <div class="box-navbar">
+                    <div class="logo">
+                        <h1>Bakery Berkah</h1>
+                    </div>
+                    <ul class="menu">
+                        <li><a href="#home">HOME</a></li>
+                        <li><a href="#service">SERVICE</a></li>
+                        <li><a href="#pantai">PRODUCT</a></li>
+                        <li><a href="#daftar">PROMO</a></li>
+                    </ul>
+                    <i class="fa-solid fa-bars menu-bar"></i>
+                </div>
+            </div>
+        </div>
+        <div class="hero">
+            <div class="container">
+                <div class="box-hero">
+                    <div class="box">
+                        <h1>Roti Pilihan Terbaik <br>
+                            Kado Spesial Untuk Keluarga</h1>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing
+                            elit. Sint, alias!</p>
+                        <button>Selengkapnya</button>
+                    </div>
+                    <div class="box">
+                        <img src="https://images.pexels.com/photos/3071821/pexels-photo-3071821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" style="border-radius: 10%;">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- NAVBAR END -->
+
+
+
+
+
+    <!-- LINK JAVASCRIPT -->
+    <script src='script.js'></script>
+</body>
+
+</html>
