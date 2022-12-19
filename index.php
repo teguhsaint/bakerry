b9183b99cedbba0c1e54487f6663476c6e05c623
husnunn <118254419+husnunn@users.noreply.github.com>
2022-12-19 14:43:13 +0700

    RIYAN

--- index.php
+++ index.php
@@ -81,51 +81,7 @@
     <!-- SERVICE END -->
 
     <!-- PRODUCT START -->
-    <div class="product" id="product">
-        <div class="container">
-            <div class="jdl">
-                <H1>PRODUCT</H1>
-            </div>
-            <div class="box-product">
-                <div class="box">
-                    <img src="https://images.pexels.com/photos/2878736/pexels-photo-2878736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
-                    <h3>Kue Ulang Tahun</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aut.</p>
-                    <button>Detail</button>
-                </div>
-                <div class="box">
-                    <img src="https://images.pexels.com/photos/2878736/pexels-photo-2878736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
-                    <h3>Kue Bolu</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aut.</p>
-                    <button>Detail</button>
-                </div>
-                <div class="box">
-                    <img src="https://images.pexels.com/photos/2878736/pexels-photo-2878736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
-                    <h3>Kue Tar</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aut.</p>
-                    <button>Detail</button>
-                </div>
-                <div class="box">
-                    <img src="https://images.pexels.com/photos/2878736/pexels-photo-2878736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
-                    <h3>Brownis</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aut.</p>
-                    <button>Detail</button>
-                </div>
-                <div class="box">
-                    <img src="https://images.pexels.com/photos/2878736/pexels-photo-2878736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
-                    <h3>Roti Gulung</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aut.</p>
-                    <button>Detail</button>
-                </div>
-                <div class="box">
-                    <img src="https://images.pexels.com/photos/2878736/pexels-photo-2878736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
-                    <h3>Roti Tawar</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aut.</p>
-                    <button>Detail</button>
-                </div>
-            </div>
-        </div>
-    </div>
+
     <!-- PRODUCT END -->
 
     <!-- PEMESANAN START -->
@@ -134,7 +90,51 @@
     <!-- PEMESANAN END -->
 
     <!-- MY TEAM START -->
-
+    <div class="myteam">
+        <div class="container">
+            <div class="jdl">
+                <h1>MY TEAM</h1>
+            </div>
+            <div class="box-myteam">
+                <div class="box">
+                    <img src="https://scontent.fsub8-1.fna.fbcdn.net/v/t1.18169-9/19732287_250219268805462_2782260732444763038_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_eui2=AeFvgu0nVvsc4djU8kXCY5tPBvmvDFmlaJsG-a8MWaVom-RYvXjlS2I0agtQaIiINgSZeL82a_aNr1td7s6O7by1&_nc_ohc=3_lYJ2forIwAX_lzBM3&_nc_ht=scontent.fsub8-1.fna&oh=00_AfD03fAliL06mrHoCfcjDbyDHmK7GTl8BPD27widPqn2Yg&oe=63C76FAF" style="border-radius: 15px;" alt="">
+                    <h3 style="color: white;">RIYAN</h3>
+                    <p style="color: white;">2202041106</p>
+                    <button><i class="fa-brands fa-whatsapp"></i></button>
+                    <button><i class="fa-brands fa-instagram"></i></button>
+                    <button><i class="fa-brands fa-facebook"></i></button>
+                    <button><i class="fa-brands fa-youtube"></i></button>
+                </div>
+                <div class="box">
+                    <img src="https://scontent.fsub8-2.fna.fbcdn.net/v/t1.18169-9/20431315_115086512477076_9179856307320647405_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeHGyEe_4WnmBfPq7xyMhr78gYhTjcit4n6BiFONyK3ifgN6LkMbQCrQCSpqwYkU4pnvGwe_gjYBu-UqS7SW13GV&_nc_ohc=PIr751l9sqkAX_CFs79&_nc_ht=scontent.fsub8-2.fna&oh=00_AfAKg0axb3dNmZj05HQE971XBmOZEJ8fdaAAXz2ExP_ZZg&oe=63C76417" style="border-radius: 15px;" alt="">
+                    <h3 style="color: white;">HUSNUN</h3>
+                    <p style="color: white;">2353642</p>
+                    <button><i class="fa-brands fa-whatsapp"></i></button>
+                    <button><i class="fa-brands fa-instagram"></i></button>
+                    <button><i class="fa-brands fa-facebook"></i></button>
+                    <button><i class="fa-brands fa-youtube"></i></button>
+                </div>
+                <div class="box">
+                    <img src="https://scontent.fsub8-2.fna.fbcdn.net/v/t1.18169-9/20431315_115086512477076_9179856307320647405_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeHGyEe_4WnmBfPq7xyMhr78gYhTjcit4n6BiFONyK3ifgN6LkMbQCrQCSpqwYkU4pnvGwe_gjYBu-UqS7SW13GV&_nc_ohc=PIr751l9sqkAX_CFs79&_nc_ht=scontent.fsub8-2.fna&oh=00_AfAKg0axb3dNmZj05HQE971XBmOZEJ8fdaAAXz2ExP_ZZg&oe=63C76417" style="border-radius: 15px;" alt="">
+                    <h3 style="color: white;">JEJE</h3>
+                    <p style="color: white;">6475838282</p>
+                    <button><i class="fa-brands fa-whatsapp"></i></button>
+                    <button><i class="fa-brands fa-instagram"></i></button>
+                    <button><i class="fa-brands fa-facebook"></i></button>
+                    <button><i class="fa-brands fa-youtube"></i></button>
+                </div>
+                <div class="box">
+                    <img src="https://scontent.fsub8-2.fna.fbcdn.net/v/t1.18169-9/20431315_115086512477076_9179856307320647405_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeHGyEe_4WnmBfPq7xyMhr78gYhTjcit4n6BiFONyK3ifgN6LkMbQCrQCSpqwYkU4pnvGwe_gjYBu-UqS7SW13GV&_nc_ohc=PIr751l9sqkAX_CFs79&_nc_ht=scontent.fsub8-2.fna&oh=00_AfAKg0axb3dNmZj05HQE971XBmOZEJ8fdaAAXz2ExP_ZZg&oe=63C76417" style="border-radius: 15px;" alt="">
+                    <h3 style="color: white;">JANCOKKKK</h3>
+                    <p style="color: white;">NIM BELUM TERSEDIA</p>
+                    <button><i class="fa-brands fa-whatsapp"></i></button>
+                    <button><i class="fa-brands fa-instagram"></i></button>
+                    <button><i class="fa-brands fa-facebook"></i></button>
+                    <button><i class="fa-brands fa-youtube"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!-- MY TEAM END -->
 
