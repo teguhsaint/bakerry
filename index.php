7914fa1ff7fc1b56180f9d6a9e381b87ac824ad8
husnunn <118254419+husnunn@users.noreply.github.com>
2022-12-19 12:23:37 +0700

    blokk

--- index.php
+++ index.php
@@ -25,9 +25,9 @@
                     <ul class="menu">
                         <li><a href="#home">HOME</a></li>
                         <li><a href="#service">SERVICE</a></li>
-                        <li><a href="#pantai">PRODUCT</a></li>
-                        <li><a href="#daftar">PROMO</a></li>
-                        <li><a href="#daftar">PEMESANAN</a></li>
+                        <li><a href="#product">PRODUCT</a></li>
+                        <li><a href="#pemesanan">PEMESANAN</a></li>
+                        <li><a href="#myteam">MY TEAM</a></li>
                     </ul>
                     <i class="fa-solid fa-bars menu-bar"></i>
                 </div>
@@ -51,6 +51,108 @@
         </div>
     </header>
     <!-- NAVBAR END -->
+
+    <!-- SERVICE START -->
+    <div class="service" id="service" data-aos="fade-up" data-aos-duration="1000">
+        <div class="container">
+            <div class="jdl">
+                <h1>SERVICE</h1>
+            </div>
+            <div class="box-service">
+                <div class="box">
+                    <i class="fa-solid fa-coins"></i>
+                    <h4>Harga Terjangkau</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, optio.</p>
+                </div>
+                <div class="box">
+                    <i class="fa-solid fa-certificate"></i>
+                    <h4>Sudah Terverifikasi</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, optio.</p>
+                </div>
+                <div class="box">
+                    <i class="fa-solid fa-people-roof"></i>
+                    <h4>Aman dan Terpercaya</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, optio.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- SERVICE END -->
+
+    <!-- PRODUCT START -->
+    <div class="product" id="product">
+        <div class="container">
+            <div class="jdl">
+                <H1>PRODUCT</H1>
+            </div>
+            <div class="box-product">
+                <div class="box">
+                    <img src="https://images.pexels.com/photos/2878736/pexels-photo-2878736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                    <h3>Kue Ulang Tahun</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aut.</p>
+                    <button>Detail</button>
+                </div>
+                <div class="box">
+                    <img src="https://images.pexels.com/photos/2878736/pexels-photo-2878736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                    <h3>Kue Bolu</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aut.</p>
+                    <button>Detail</button>
+                </div>
+                <div class="box">
+                    <img src="https://images.pexels.com/photos/2878736/pexels-photo-2878736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                    <h3>Kue Tar</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aut.</p>
+                    <button>Detail</button>
+                </div>
+                <div class="box">
+                    <img src="https://images.pexels.com/photos/2878736/pexels-photo-2878736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                    <h3>Brownis</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aut.</p>
+                    <button>Detail</button>
+                </div>
+                <div class="box">
+                    <img src="https://images.pexels.com/photos/2878736/pexels-photo-2878736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                    <h3>Roti Gulung</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aut.</p>
+                    <button>Detail</button>
+                </div>
+                <div class="box">
+                    <img src="https://images.pexels.com/photos/2878736/pexels-photo-2878736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                    <h3>Roti Tawar</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aut.</p>
+                    <button>Detail</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- PRODUCT END -->
+
+    <!-- PEMESANAN START -->
+
+
+    <!-- PEMESANAN END -->
+
+    <!-- MY TEAM START -->
+
+
+    <!-- MY TEAM END -->
+
+    <!-- FOOTER START -->
+    <div class="footer">
+        <div class="container">
+            <div class="box-footer">
+                <div class="box">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.5001134821105!2d112.22967907425253!3d-7.520299774205788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e783fe13394c7d1%3A0xc62ce7791d01c2cd!2sUniversitas%20KH.%20Abdul%20Wahab%20Hasbullah!5e0!3m2!1sid!2sid!4v1671427100211!5m2!1sid!2sid" width="200" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="box">
+                    <p>&copy; Copyright by <span> Bakery Berkah </span> All Rights Reserved 2022, Indonesia</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- FOOTER END -->
 
 
 
